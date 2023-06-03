22e058d675afb4e02fd4379639cb9da61c57fa27
Kenta Kubo <kkk669@users.noreply.github.com>
2023-06-03 18:25:23 +0900

    Add public Attribute.init

--- Sources/Tokenizer/Attribute.swift
+++ Sources/Tokenizer/Attribute.swift
@@ -1,6 +1,11 @@
 public struct Attribute {
     public var name: String
     public var value: String
+
+    public init(name: __owned String, value: __owned String) {
+        self.name = _move name
+        self.value = _move value
+    }
 }
 
 extension Attribute: Equatable {}
