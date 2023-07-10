567d585aef1e122d320e3a881c4a4e9477d4e396
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2023-07-11 00:12:59 +0900

    Add test

---
+++ Tests/TokenizerTests/TokenizerTests.swift
@@ -0,0 +1,79 @@
+import XCTest
+
+@testable import Tokenizer
+
+struct TestSink {
+    var tokens = [Token]()
+}
+
+extension TestSink: TokenSink {
+    mutating func process(_ token: __owned Token) {
+        self.tokens.append(consume token)
+    }
+}
+
+final class TokenizerTests: XCTestCase {
+    func testTokenizeBasicHTML() {
+        let html = #"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <title>title</title>
+        </head>
+        <body>
+        hi
+        </body>
+        </html>
+        """#
+
+        let sink = TestSink()
+        var tokenizer = Tokenizer(sink: consume sink)
+        var iter = html.makeIterator()
+        tokenizer.tokenize(&iter)
+
+        let tokens: [Token] = [
+            .doctype(.init(name: "html")),
+            "\n",
+            .tag(
+                Tag(
+                    name: "html",
+                    kind: .start,
+                    attrs: [.init(name: "lang", value: "en")]
+                )
+            ),
+            "\n",
+            .tag(Tag(name: "head", kind: .start)),
+            "\n",
+            .tag(
+                Tag(
+                    name: "meta",
+                    kind: .start,
+                    attrs: [.init(name: "charset", value: "UTF-8")],
+                    selfClosing: true
+                )
+            ),
+            "\n",
+            .tag(Tag(name: "title", kind: .start)),
+            "t",
+            "i",
+            "t",
+            "l",
+            "e",
+            .tag(Tag(name: "title", kind: .end)),
+            "\n",
+            .tag(Tag(name: "head", kind: .end)),
+            "\n",
+            .tag(Tag(name: "body", kind: .start)),
+            "\n",
+            "h",
+            "i",
+            "\n",
+            .tag(Tag(name: "body", kind: .end)),
+            "\n",
+            .tag(Tag(name: "html", kind: .end)),
+            .eof,
+        ]
+        XCTAssertEqual(tokens, tokenizer.sink.tokens)
+    }
+}
