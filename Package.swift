3b835e1db7d69b6c088f4a57f6740bc3ff9df54c
Kenta Kubo <kkk669@users.noreply.github.com>
2023-09-22 03:35:02 +0900

    Introduce swift-testing

--- Package.swift
+++ Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "zyphy",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         .library(name: "Tokenizer", targets: ["Tokenizer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0")
+        .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
     ],
     targets: [
         .target(
@@ -36,20 +37,19 @@ let package = Package(
                 .unsafeFlags(["-Xfrontend", "-warn-long-expression-type-checking=100"], .when(configuration: .debug)),
                 .unsafeFlags(["-swift-version", "6"]),
             ]
-
         ),
         .testTarget(
             name: "TokenizerTests",
             dependencies: [
                 "TokenizerMacros",
                 "Tokenizer",
+                .product(name: "Testing", package: "swift-testing"),
             ],
             swiftSettings: [
                 .unsafeFlags(["-Xfrontend", "-warn-long-function-bodies=100"], .when(configuration: .debug)),
                 .unsafeFlags(["-Xfrontend", "-warn-long-expression-type-checking=100"], .when(configuration: .debug)),
                 .unsafeFlags(["-swift-version", "6"]),
             ]
-
         ),
     ]
 )
--- Tests/TokenizerTests/TokenizerTests.swift
+++ Tests/TokenizerTests/TokenizerTests.swift
@@ -1,4 +1,4 @@
-import XCTest
+import Testing
 
 @testable import Tokenizer
 
@@ -12,69 +12,54 @@ extension TestSink: TokenSink {
     }
 }
 
-final class TokenizerTests: XCTestCase {
-    func testTokenizeBasicHTML() {
-        let html = #"""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-            <meta charset="UTF-8" />
-            <title>title</title>
-            </head>
-            <body>
-            hi
-            </body>
-            </html>
-            """#
+@Test func basicHTML() {
+    let html = #"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <title>title</title>
+        </head>
+        <body>
+        hi
+        </body>
+        </html>
+        """#
 
-        let sink = TestSink()
-        var tokenizer = Tokenizer(sink: sink)
-        var iter = html.makeIterator()
-        tokenizer.tokenize(&iter)
+    let sink = TestSink()
+    var tokenizer = Tokenizer(sink: sink)
+    var iter = html.makeIterator()
+    tokenizer.tokenize(&iter)
 
-        let tokens: [Token] = [
-            .doctype(.init(name: "html")),
-            "\n",
-            .tag(
-                Tag(
-                    name: "html",
-                    kind: .start,
-                    attrs: [.init(name: "lang", value: "en")]
-                )
-            ),
-            "\n",
-            .tag(Tag(name: "head", kind: .start)),
-            "\n",
-            .tag(
-                Tag(
-                    name: "meta",
-                    kind: .start,
-                    attrs: [.init(name: "charset", value: "UTF-8")],
-                    selfClosing: true
-                )
-            ),
-            "\n",
-            .tag(Tag(name: "title", kind: .start)),
-            "t",
-            "i",
-            "t",
-            "l",
-            "e",
-            .tag(Tag(name: "title", kind: .end)),
-            "\n",
-            .tag(Tag(name: "head", kind: .end)),
-            "\n",
-            .tag(Tag(name: "body", kind: .start)),
-            "\n",
-            "h",
-            "i",
-            "\n",
-            .tag(Tag(name: "body", kind: .end)),
-            "\n",
-            .tag(Tag(name: "html", kind: .end)),
-            .eof,
-        ]
-        let result = tokenizer.sink.tokens
-        XCTAssertEqual(result, tokens)
-    }
+    let tokens: [Token] = [
+        .doctype(.init(name: "html")),
+        "\n",
+        .tag(Tag(name: "html", kind: .start, attrs: [.init(name: "lang", value: "en")])),
+        "\n",
+        .tag(Tag(name: "head", kind: .start)),
+        "\n",
+        .tag(Tag(name: "meta", kind: .start, attrs: [.init(name: "charset", value: "UTF-8")], selfClosing: true)),
+        "\n",
+        .tag(Tag(name: "title", kind: .start)),
+        "t",
+        "i",
+        "t",
+        "l",
+        "e",
+        .tag(Tag(name: "title", kind: .end)),
+        "\n",
+        .tag(Tag(name: "head", kind: .end)),
+        "\n",
+        .tag(Tag(name: "body", kind: .start)),
+        "\n",
+        "h",
+        "i",
+        "\n",
+        .tag(Tag(name: "body", kind: .end)),
+        "\n",
+        .tag(Tag(name: "html", kind: .end)),
+        .eof,
+    ]
+    let result = tokenizer.sink.tokens
+    #expect(result == tokens)
 }
