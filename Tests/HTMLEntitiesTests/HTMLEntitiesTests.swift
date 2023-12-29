e431c072aa4a9cc8dc7884d3014438f1b19cc702
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2023-12-29 23:47:49 +0900

    refactor: refactor HTMLEntitiesTests.swift

--- Tests/HTMLEntitiesTests/HTMLEntitiesTests.swift
+++ Tests/HTMLEntitiesTests/HTMLEntitiesTests.swift
@@ -2,24 +2,22 @@ import Foundation
 private import HTMLEntities
 import Testing
 
+private struct Entry: Decodable {
+    var codepoints: [UInt32]
+}
+
 // swift-format-ignore: NeverForceUnwrap
 @Test func namedCharRef() throws {
-    struct Entry: Decodable {
-        var codepoints: [UInt32]
-    }
-
     let dict = try JSONDecoder()
         .decode(
             [String: Entry].self,
             from: Data(contentsOf: Bundle.module.url(forResource: "entities", withExtension: "json")!)
         )
 
-    for (k, v) in dict {
-        let (c1, c2) = namedChars[String(k.dropFirst())]!
-        if c2.value != 0 {
-            #expect(v.codepoints == [c1.value, c2.value])
-        } else {
-            #expect(v.codepoints == [c1.value])
+    for (key, value) in dict {
+        switch namedChars[String(key.dropFirst())]! {
+        case (let c1, "\0"): #expect(value.codepoints == [c1.value])
+        case (let c1, let c2): #expect(value.codepoints == [c1.value, c2.value])
         }
     }
 }
