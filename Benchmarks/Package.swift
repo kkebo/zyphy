bfca403aad047277a6fb3e1b9ba6ec8216116117
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-03-14 00:52:59 +0900

    build(benchmark): bump package-benchmark to 1.22.4

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.macOS(.v13), .iOS(.v16)],
     dependencies: [
         .package(name: "zyphy", path: ".."),
-        .package(url: "https://github.com/ordo-one/package-benchmark", from: "1.22.3"),
+        .package(url: "https://github.com/ordo-one/package-benchmark", from: "1.22.4"),
     ],
     targets: [
         .executableTarget(
