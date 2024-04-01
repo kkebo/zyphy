d0e7ac6a3b8a52b8b8b289830c4b6d9a842e6e16
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-04-02 02:02:55 +0900

    fix: fix compiler warnings

--- Tests/TokenizerTests/Bundle+Sendable.swift
+++ Tests/TokenizerTests/Bundle+Sendable.swift
@@ -1,4 +1,4 @@
 private import Foundation
 
 // FIXME: This is a workaround for "error: static property 'module' is not concurrency-safe because non-'Sendable' type 'Bundle' may have shared mutable state"
-extension Bundle: @unchecked Sendable {}
+extension Bundle: @retroactive @unchecked Sendable {}
