b9184fe05bfa6dcc3c2d98213fbfd632420f23e7
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-04-15 00:13:50 +0900

    feat: add the TreeConstructor module
    WIP: The implementation of this module is in progress.

--- Sources/TreeConstructor/TreeConstructor.swift
+++ Sources/TreeConstructor/TreeConstructor.swift
@@ -10,6 +10,58 @@ public struct TreeConstructor {
 
 extension TreeConstructor: TokenSink {
     mutating func process(_ token: consuming Token) {
-        // TODO: Implement here
+        switch self.mode {
+        case .initial:
+            switch token {
+            case .char("\t"), .char("\n"), .char("\u{000C}"), .char("\r"), .char(" "): break
+            case .comment(_): fatalError("not implemented")
+            case .doctype(_): fatalError("not implemented")
+            case _: fatalError("not implemented")
+            }
+        case .beforeHTML:
+            fatalError("not implemented")
+        case .beforeHead:
+            fatalError("not implemented")
+        case .inHead:
+            fatalError("not implemented")
+        case .inHeadNoscript:
+            fatalError("not implemented")
+        case .afterHead:
+            fatalError("not implemented")
+        case .inBody:
+            fatalError("not implemented")
+        case .text:
+            fatalError("not implemented")
+        case .inTable:
+            fatalError("not implemented")
+        case .inTableText:
+            fatalError("not implemented")
+        case .inCaption:
+            fatalError("not implemented")
+        case .inColumnGroup:
+            fatalError("not implemented")
+        case .inTableBody:
+            fatalError("not implemented")
+        case .inRow:
+            fatalError("not implemented")
+        case .inCell:
+            fatalError("not implemented")
+        case .inSelect:
+            fatalError("not implemented")
+        case .inSelectInTable:
+            fatalError("not implemented")
+        case .inTemplate:
+            fatalError("not implemented")
+        case .afterBody:
+            fatalError("not implemented")
+        case .inFrameset:
+            fatalError("not implemented")
+        case .afterFrameset:
+            fatalError("not implemented")
+        case .afterAfterBody:
+            fatalError("not implemented")
+        case .afterAfterFrameset:
+            fatalError("not implemented")
+        }
     }
 }
