e2323ffe4f5c99b4cdb6f38a81a7b7ff04c28dc2
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-05-23 06:24:59 +0900

    perf: improve performance of `BufferQueue.pop`

--- Sources/Tokenizer/BufferQueue.swift
+++ Sources/Tokenizer/BufferQueue.swift
@@ -24,16 +24,14 @@ public struct BufferQueue: ~Copyable, Sendable {
         return self.buffers[0].popFirst()
     }
 
+    @inline(__always)
     mutating func pop(except s: consuming SmallCharSet) -> PopResult? {
         guard !self.buffers.isEmpty else { return nil }
         defer { if self.buffers[0].isEmpty { self.buffers.removeFirst() } }
-        let others = self.buffers[0].prefix { $0.value >= 64 || !s.contains($0) }
-        self.buffers[0].removeFirst(others.count)
-        return if others.isEmpty {
-            self.buffers[0].popFirst().map(PopResult.known)
-        } else {
-            .others(others)
-        }
+        let count = (self.buffers[0].firstIndex { $0.value < 64 && s.contains($0) } ?? self.buffers[0].endIndex) - self.buffers[0].startIndex
+        guard count > 0 else { return self.buffers[0].popFirst().map(PopResult.known) }
+        defer { self.buffers[0].removeFirst(count) }
+        return .others(self.buffers[0].prefix(count))
     }
 
     mutating func removeFirst() {
