6369ee95f40bca68abd5d24c573683aca88d173d
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-08-13 05:26:29 +0900

    test: replace `String` with `Str`

--- Sources/Tokenizer/Tag.swift
+++ Sources/Tokenizer/Tag.swift
@@ -17,22 +17,4 @@ public struct Tag: Equatable, Sendable {
         self.attrs = attrs
         self.selfClosing = selfClosing
     }
-
-    // For testing
-    package init(
-        name: consuming String,
-        kind: consuming TagKind,
-        attrs: consuming [String: String] = [:],
-        selfClosing: Bool = false
-    ) {
-        self.name = Str(name.unicodeScalars)
-        self.kind = kind
-        var newAttrs: [Str: Str] = [:]
-        for (k, v) in attrs {
-            newAttrs[Str(k.unicodeScalars)] = Str(v.unicodeScalars)
-        }
-        self.attrs = newAttrs
-        self.selfClosing = selfClosing
-    }
-
 }
