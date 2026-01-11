2d71c1790da2409326a48891008b03cce6a2f7ee
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-01-12 01:43:14 +0900

    perf: remove `@inline(always) from large functions

--- Sources/Tokenizer/SmallCharSet.swift
+++ Sources/Tokenizer/SmallCharSet.swift
@@ -1,19 +1,16 @@
-public import Str
+import Str
 
-@usableFromInline
 struct SmallCharSet {
-    @usableFromInline
     var bits: UInt64
 
     @inline(always)
-    @export(implementation)
+    @inlinable
     func contains(_ c: Char) -> Bool {
         self.bits & 1 << c.value != 0
     }
 }
 
 extension SmallCharSet: ExpressibleByArrayLiteral {
-    @usableFromInline
     init(arrayLiteral elements: Char...) {
         self.bits = elements.lazy.map { 1 << $0.value }.reduce(0, |)
     }
